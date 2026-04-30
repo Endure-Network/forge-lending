a9b267521ed7102a520e3563ea91fa6bc5cd69ed
Apeguru <hello@apeguru.dev>
2026-04-30 20:08:50 -0300

    feat: wire optional prime deploy script
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- packages/deploy/src/DeployWithOptionals.s.sol
+++ packages/deploy/src/DeployWithOptionals.s.sol
@@ -23,7 +23,7 @@ contract DeployWithOptionals is Script {
         EndureDeployHelper.Addresses memory addr = helper.deployAll();
 
         address xvs;
-        if (enableXVS) {
+        if (enableXVS || enablePrime) {
             xvs = _enableXVSRewards(helper, addr, deployer);
         }
 
@@ -38,11 +38,14 @@ contract DeployWithOptionals is Script {
             liquidatorAddresses = _enableLiquidator(helper, addr);
         }
 
-        require(!enablePrime, "DeployWithOptionals: ENABLE_PRIME not implemented yet");
+        EndureDeployHelper.PrimeAddresses memory primeAddresses;
+        if (enablePrime) {
+            primeAddresses = _enablePrime(helper, addr, xvs);
+        }
 
         vm.stopBroadcast();
 
-        _writeAddresses(addr, xvs, vaiAddresses, liquidatorAddresses, enableVAI, enableLiquidator, enablePrime);
+        _writeAddresses(addr, xvs, vaiAddresses, liquidatorAddresses, primeAddresses, enableVAI, enableLiquidator, enablePrime);
     }
 
     function _enableXVSRewards(
@@ -106,11 +109,51 @@ contract DeployWithOptionals is Script {
         return helper.deployLiquidatorOptional(addr, config);
     }
 
+    function _enablePrime(
+        EndureDeployHelper helper,
+        EndureDeployHelper.Addresses memory addr,
+        address xvs
+    ) internal returns (EndureDeployHelper.PrimeAddresses memory primeAddresses) {
+        address[] memory markets = new address[](1);
+        markets[0] = addr.vWTAO;
+        uint256[] memory supplyMultipliers = new uint256[](1);
+        supplyMultipliers[0] = vm.envOr("PRIME_VWTAO_SUPPLY_MULTIPLIER", uint256(1e18));
+        uint256[] memory borrowMultipliers = new uint256[](1);
+        borrowMultipliers[0] = vm.envOr("PRIME_VWTAO_BORROW_MULTIPLIER", uint256(1e18));
+
+        EndureDeployHelper.PrimeConfig memory config = EndureDeployHelper.PrimeConfig({
+            blocksPerYear: vm.envOr("PRIME_BLOCKS_PER_YEAR", uint256(100)),
+            stakingPeriod: vm.envOr("PRIME_STAKING_PERIOD", uint256(600)),
+            minimumStakedXVS: vm.envOr("PRIME_MINIMUM_STAKED_XVS", uint256(1000e18)),
+            maximumXVSCap: vm.envOr("PRIME_MAXIMUM_XVS_CAP", uint256(100000e18)),
+            xvsVaultPoolId: vm.envOr("PRIME_XVS_VAULT_POOL_ID", uint256(0)),
+            xvsVaultRewardPerBlock: vm.envOr("PRIME_XVS_VAULT_REWARD_PER_BLOCK", uint256(1e18)),
+            xvsVaultLockPeriod: vm.envOr("PRIME_XVS_VAULT_LOCK_PERIOD", uint256(300)),
+            alphaNumerator: uint128(vm.envOr("PRIME_ALPHA_NUMERATOR", uint256(1))),
+            alphaDenominator: uint128(vm.envOr("PRIME_ALPHA_DENOMINATOR", uint256(2))),
+            loopsLimit: vm.envOr("PRIME_LOOPS_LIMIT", uint256(20)),
+            irrevocableLimit: vm.envOr("PRIME_IRREVOCABLE_LIMIT", uint256(1000)),
+            revocableLimit: vm.envOr("PRIME_REVOCABLE_LIMIT", uint256(1000)),
+            primeMarkets: markets,
+            supplyMultipliers: supplyMultipliers,
+            borrowMultipliers: borrowMultipliers
+        });
+
+        EndureDeployHelper.PrimeBytecode memory bytecode = EndureDeployHelper.PrimeBytecode({
+            xvsVaultProxyCreationCode: vm.getCode("../contracts/out/XVSVaultProxy.sol/XVSVaultProxy.0.5.16.json"),
+            xvsVaultCreationCode: vm.getCode("../contracts/out/XVSVault.sol/XVSVault.json"),
+            xvsStoreCreationCode: vm.getCode("../contracts/out/XVSStore.sol/XVSStore.json")
+        });
+
+        return helper.deployPrimeOptional(addr, xvs, config, bytecode);
+    }
+
     function _writeAddresses(
         EndureDeployHelper.Addresses memory addr,
         address xvs,
         EndureDeployHelper.VAIAddresses memory vaiAddresses,
         EndureDeployHelper.LiquidatorAddresses memory liquidatorAddresses,
+        EndureDeployHelper.PrimeAddresses memory primeAddresses,
         bool enableVAI,
         bool enableLiquidator,
         bool enablePrime
@@ -140,8 +183,13 @@ contract DeployWithOptionals is Script {
         vm.serializeAddress(json, "liquidator", liquidatorAddresses.liquidator);
         vm.serializeAddress(json, "liquidatorImplementation", liquidatorAddresses.liquidatorImplementation);
         vm.serializeAddress(json, "protocolShareReserve", liquidatorAddresses.protocolShareReserve);
-        vm.serializeAddress(json, "prime", address(0));
-        vm.serializeAddress(json, "primeLiquidityProvider", address(0));
+        vm.serializeAddress(json, "prime", primeAddresses.prime);
+        vm.serializeAddress(json, "primeImplementation", primeAddresses.primeImplementation);
+        vm.serializeAddress(json, "primeLiquidityProvider", primeAddresses.primeLiquidityProvider);
+        vm.serializeAddress(json, "primeLiquidityProviderImplementation", primeAddresses.primeLiquidityProviderImplementation);
+        vm.serializeAddress(json, "xvsVault", primeAddresses.xvsVault);
+        vm.serializeAddress(json, "xvsVaultImplementation", primeAddresses.xvsVaultImplementation);
+        vm.serializeAddress(json, "xvsStore", primeAddresses.xvsStore);
         vm.serializeBool(json, "enableVAI", enableVAI);
         vm.serializeBool(json, "enableLiquidator", enableLiquidator);
         string memory finalJson = vm.serializeBool(json, "enablePrime", enablePrime);
